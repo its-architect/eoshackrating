522b9d1715c1ba040e1c804ef55d6ac38250ce43
Ivan Lukyanov <iv@itsphere.io>
2018-06-10 02:05:36 +0800

    Check

--- back-end/app.js
+++ back-end/app.js
@@ -4,8 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var authRouter = require('./routes/auth');
 var apiRouter = require('./routes/api');
 
 var app = express();
@@ -20,9 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/auth', authRouter);
-app.use('/api', apiRouter);
+app.use('/', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
