39248cb699dbf7f65e2dddca3f9f0178741747a5
Arseniy-II <arseniy.public@gmail.com>
2018-06-09 10:10:34 +0200

    front-end add user page

--- front-end/src/components/shared/index.js
+++ front-end/src/components/shared/index.js
@@ -1,9 +1,13 @@
 import RedirectRouteComponent from './RedirectRouteComponent';
-import SpinnerComponent       from './SpinnerComponent';
-import ScoreComponent         from './ScoreComponent';
-import ClickOutside           from './ClickOutside';
+import SpinnerComponent from './SpinnerComponent';
+import ScoreComponent from './ScoreComponent';
+import ClickOutside from './ClickOutside';
+import AvatarComponent from './AvatarComponent';
+import RatingComponent from './RatingComponent';
 
 export {
+    RatingComponent,
+    AvatarComponent,
     ClickOutside,
     ScoreComponent,
     SpinnerComponent,
--- front-end/src/ducks/app/selectors.js
+++ front-end/src/ducks/app/selectors.js
@@ -1,5 +1,4 @@
 export default {
-    selectQueryId: ( state ) => ({ id: state.getIn([ 'app', 'locationBeforeTransitions', 'query', 'id' ]) }),
     selectPath: ( state ) => ({ path: state.getIn([ 'app', 'locationBeforeTransitions', 'pathname' ]) }),
-    selectQuery: ( state ) => ({ query: state.getIn([ 'app', 'locationBeforeTransitions', 'query' ]) }),
+    selectQueryParam: ( state, queryParam ) => (state.getIn([ 'app', 'locationBeforeTransitions', 'query' ])[ queryParam ]),
 };
